0c25732ba70fa28d62238903baf2af128c7af7e7
Michael Clayton <michael.clayton@procore.com>
2019-12-10 14:46:48 -0800

    Add description markdown

--- readme_gen/generate.sh
+++ readme_gen/generate.sh
@@ -4,6 +4,7 @@
 # Generates the README #
 ########################
 GITHUB_ROOT="https://github.com/mcclayton/TSCodeChallenges/tree/master"
+DESCRIPTION_FILENAME="description.md"
 
 cat ./readme_gen/README_TITLE.md > README.md   # Add README_TITLE to README
 
@@ -13,7 +14,7 @@ find ./src -type f -name "index.ts" | while read path; do
   SOLUTION_PATH="$GITHUB_ROOT/$path"
   DIRECTORY_PATH="$(dirname $path)"
   DIRECTORY_NAME="$(basename "$DIRECTORY_PATH")"
-  DESCRIPTION="$(cat $DIRECTORY_PATH/description.txt)"
+  DESCRIPTION="$(cat $DIRECTORY_PATH/$DESCRIPTION_FILENAME)"
   echo "| [$DIRECTORY_NAME]($SOLUTION_PATH) | $DESCRIPTION |" >> README.md
 done
 
