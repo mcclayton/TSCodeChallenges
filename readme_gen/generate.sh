38bff8dcdd9cd1f8bd509d8907af86c5bd90f666
Michael Clayton <michael.clayton@procore.com>
2019-12-13 20:44:26 -0800

    Update readme gen

--- readme_gen/generate.sh
+++ readme_gen/generate.sh
@@ -4,18 +4,23 @@
 # Generates the README #
 ########################
 GITHUB_ROOT="https://github.com/mcclayton/TSCodeChallenges/tree/master"
-DESCRIPTION_FILENAME="description.md"
+PROBLEM_FILENAME="problem.md"
+INFO_FILENAME="info.md"
 
 cat ./readme_gen/README_TITLE.md > README.md   # Add README_TITLE to README
 
-echo "| Solution | Description |" >> README.md
-echo "|:---------:|:---------:|" >> README.md
+echo "| Code | Description | Solution |" >> README.md
+echo "|:---------:|:---------:|:---------:|" >> README.md
 find ./src -type f -name "index.ts" | while read path; do
   SOLUTION_PATH="$GITHUB_ROOT/$path"
   DIRECTORY_PATH="$(dirname $path)"
   DIRECTORY_NAME="$(basename "$DIRECTORY_PATH")"
-  DESCRIPTION="$(cat $DIRECTORY_PATH/$DESCRIPTION_FILENAME 2>/dev/null)"
-  echo "| [$DIRECTORY_NAME]($SOLUTION_PATH) | $DESCRIPTION |" >> README.md
+
+  CODE_SOL_LINK="[$DIRECTORY_NAME]($SOLUTION_PATH)"
+  PROBLEM_MD="$(cat $DIRECTORY_PATH/$PROBLEM_FILENAME 2>/dev/null)"
+  INFO_MD="$(cat $DIRECTORY_PATH/$INFO_FILENAME 2>/dev/null)"
+
+  echo "| $CODE_SOL_LINK | $PROBLEM_MD | $INFO_MD |" >> README.md
 done
 
 # Print Out Resulting README
---
+++ src/LargestNumber/info.md
@@ -0,0 +1,1 @@
+[Leetcode](https://leetcode.com/articles/largest-number/) & [GeeksforGeeks](https://www.geeksforgeeks.org/given-an-array-of-numbers-arrange-the-numbers-to-form-the-biggest-number/)
