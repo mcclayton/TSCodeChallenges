3016b430b21cf191dfde2ee71e3ec2f28d4f8014
Michael Clayton <michael.clayton@procore.com>
2020-01-06 10:48:27 -0800

    Add comments for two sum

--- src/solutions/TwoSum/index.ts
+++ src/solutions/TwoSum/index.ts
@@ -5,11 +5,16 @@
  * @return {number[]}
  */
 const twoSum = function (nums: number[], target: number) {
+  // Create a map { [number]: indexOfNumber }
   const map: IHash = {};
   for (let i = 0; i < nums.length; i++) {
     map[nums[i]] = i;
   }
 
+  // For each number, check to see if the compliment of the number
+  // (Example. Target: 9, Number: 7, Complement: 9 - 7 = 2)
+  // is in the hashmap. If the compliment is in the map, and isn't the current number
+  // then return the pair of indeces.
   for (let i = 0; i < nums.length; i++) {
     const compliment = target - nums[i];
     if (map[compliment] !== undefined && map[compliment] !== i) {
